808ade64171e61bb2f5b7a497e2de79c8e19c02a
Linus Gasser <ineiti@linusetviviane.ch>
2011-10-06 22:35:51 +0100

    Fixed crash in CourseModify::rpc_list_choice

--- Views/CourseModify.rb
+++ Views/CourseModify.rb
@@ -12,12 +12,11 @@
 # town - the default town
 # country - the default country
 
-
 class CourseModify < View
   def layout
     set_data_class :Courses
     @update = true
-    
+
     gui_hbox do
       gui_vbox :nogroup do
         gui_vbox :nogroup do
@@ -61,50 +60,50 @@ class CourseModify < View
       end
     end
   end
-  
+
   def rpc_button_delete( sid, data )
     dputs 3, "sid, data: #{[sid, data.inspect].join(':')}"
     course = @data_class.find_by_course_id( data['courses'][0])
     dputs 3, "Got #{course.name} - #{course.inspect}"
     if course
       dputs 2, "Deleting entry #{course}"
-      course.delete
+    course.delete
     end
-    
+
     reply( "empty", [:courses] ) +
     reply( "update", { :courses => @data_class.list_courses } )
   end
-  
+
   def rpc_button_save( sid, data )
     course = @data_class.find_by_name( data['name'] )
-    if course 
+    if course
       # BUG: they're already saved, don't save it again
       data.delete( 'students' )
-      course.set_data( data )
+    course.set_data( data )
     end
   end
-  
+
   def rpc_button_export( sid, data )
   end
-  
+
   def rpc_button_add_students( sid, data )
     reply( "window_show", "students_win" )
   end
-  
+
   def rpc_button_bulk_add( sid, data )
     if data['name']
       reply( "window_show", "students_bulk" )
     end
   end
-  
+
   def rpc_button_del_student( sid, data )
     course = @data_class.find_by_name( data['name'] )
     data['students'].each{|s|
-      course.students.delete( s)  
+      course.students.delete( s)
     }
     reply( "empty_only", [:students] ) + reply( "update", course.data )
   end
-  
+
   def rpc_button_new_student( sid, data )
     course = @data_class.find_by_name( data['name'] )
     if course
@@ -113,16 +112,16 @@ class CourseModify < View
       end
       data['students_add'].each{|s|
         if not course.students.index( s )
-          course.students.push( s )
+        course.students.push( s )
         end
       }
       dputs 3, "Students are now: #{course.students.inspect}"
-      reply( "empty_only", [:students] ) + 
+      reply( "empty_only", [:students] ) +
       reply( "update", { :students => course.list_students } ) +
       reply( "window_hide" )
     end
   end
-  
+
   # This will add a whole lot of students to the list, creating them and setting
   # the permissions to "student", but without generating a password
   # As the creation of a student can take quite some time (10s of seconds),
@@ -140,31 +139,31 @@ class CourseModify < View
       first = names[0..s].join(" ")
       family = names[(s+1)..-1].join(" ")
       dputs 1, "Creating user #{u.inspect} as #{first} - #{family}"
-      person = Entities.Persons.create( {:first_name => first, :family_name => family, 
+      person = Entities.Persons.create( {:first_name => first, :family_name => family,
         :permissions => %w( student ), :town => @town, :country => @country })
       person.email = "#{person.login_name}@ndjair.net"
       course.students.push( person.login_name )
       if defined? @cmd_after_new
-          %x[ #{@cmd_after_new} #{person.login_name} ]
+        %x[ #{@cmd_after_new} #{person.login_name} ]
       end
     end
     if users.length > 0
       reply( "update", { :names => users.join("\n") } ) +
       reply( "callback_button", "bulk_students" )
     else
-      reply( "empty_only", [:students] ) + 
+      reply( "empty_only", [:students] ) +
       reply( "update", { :students => course.list_students } ) +
-      reply( "window_hide" )      
+      reply( "window_hide" )
     end
   end
-  
+
   def rpc_button_add_course( sid, data)
     reply( "window_show", "course" )
   end
-  
+
   def rpc_button_new_course( sid, data )
     dputs 3, "sid: #{sid} - data: #{data.inspect}"
-    
+
     name = "#{data['name_base'][0]}_#{data['name_date']}"
     course = @data_class.find_by_name( name )
     if name =~ /_.+/
@@ -172,29 +171,31 @@ class CourseModify < View
         course = @data_class.create( {:name => name })
       end
     end
-    
+
     reply("empty", [:students, :courses]) +
     reply( "update", {:courses => Entities.Courses.list_courses}) +
     reply( "update", course.to_hash ) +
     reply( "update", { :courses => [ course.course_id ] }) +
     reply( "window_hide" )
   end
-  
+
   def rpc_button_close( sid, data )
     reply( "window_hide", "*" )
   end
-  
+
   def rpc_list_choice( sid, name, *args )
     #Calling rpc_list_choice with [["courses", {"courses"=>["base_25"], "name_base"=>["base"]}]]
     dputs 3, "rpc_list_choice with #{name} - #{args.inspect}"
-    if name == "courses"
+    if name == "courses" and args[0]['courses'].length > 0
       course_id = args[0]['courses'][0]
-      dputs 3, "replying"
+      dputs 3, "replying for course_id #{course_id}"
       course = @data_class.find_by_course_id(course_id)
       reply("empty", [:students]) +
       reply("update", course.to_hash ) +
       reply("update", {:courses => [course_id] } )
+    else
+      reply("empty", [:students])
     end
   end
-  
+
 end
