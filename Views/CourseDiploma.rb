96e5349d890813ebf6533ecb03951c7580ee1e19
Linus Gasser <ineiti@linusetviviane.ch>
2011-10-12 12:53:19 +0100

    on the road to grades

--- Views/CourseDiploma.rb
+++ Views/CourseDiploma.rb
@@ -1,35 +1,58 @@
 =begin
-  Shows the existing courses and the courses on the server, so that the
-  two can be synchronized.
+Shows the existing courses and the courses on the server, so that the
+two can be synchronized.
 =end
 
 class CourseDiploma < View
   def layout
     set_data_class :Courses
-    
+
     gui_hbox do
       gui_vbox :nogroup do
-        show_list_single :local, "Entities.Courses.list_courses"
-      end
-      gui_vbox do
-        show_button :to_grade
-        show_button :from_grade
+        show_list_single :courses, "Entities.Courses.list_courses", :callback => true
+        show_button :do_grades
       end
       gui_vbox :nogroup do
-        show_list_single :grade, "View.CourseDiploma.list_grade"
+        show_list_single :grade
+        show_button :print
       end
     end
   end
-  
-  def list_grade
-    %w( base_0401 base_0402 base_0403 )
+
+  def rpc_list_choice( sid, name, args )
+    dputs 3, "rpc_list_choice with #{name} - #{args.inspect}"
+    ret = reply('empty', ['grade'])
+    case name
+    when "courses"
+      if args['courses'].length > 0
+        courseDir = @diplomaDir + "/" + Entities.Courses.find_by_course_id( args['courses'] ).name
+        dputs 2, "Looking for directory #{courseDir}"
+        if File::directory?( courseDir )
+          ret += reply( 'update', :grade => Dir::entries( courseDir ) )
+        end
+      end
+    end
+    return ret
   end
-  
-  def rpc_button_to_grade( sid, data )
-    local = data['local']
-    return if local.size == 0
-    course = Entities.Courses.find_by_course_id( local )
-    dputs 0, "Data is: #{course.inspect}"
-    course.export_to_diploma
+
+  def rpc_button_do_grades( sid, args )
+    course_id = args['courses'][0]
+    course = @data_class.find_by_course_id(course_id).to_hash
+    students = course[:students]
+    digits = Math::log10( students.size + 1 ).ceil
+    counter = 1
+    courseDir = @diplomaDir + "/" + course[:name]
+    if not File::directory? courseDir
+      FileUtils::mkdir( courseDir )
+    else
+      FileUtils::rm( Dir.glob( courseDir + "/*" ) )
+    end
+    dputs 2, students.inspect
+    students.each{ |s|
+      dputs 2, "Doing #{counter}: #{s[0]}"
+      FileUtils::cp( "#{@diplomaDir}/base_gestion.odt", "#{courseDir}/#{counter.to_s.rjust(digits, '0')}.odt")
+      counter += 1
+    }
+    rpc_list_choice( sid, "courses", "courses" => course_id.to_s )
   end
 end
