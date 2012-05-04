5d37a63cd68d41a25fd59e0ebff72a27cc2be896
Linus Gasser <ineiti@linusetviviane.ch>
2012-05-04 22:07:26 +0100

    changed files for course

---
+++ Views/Course/Diploma.rb
@@ -0,0 +1,172 @@
+=begin
+Shows the existing courses and the courses on the server, so that the
+two can be synchronized.
+=end
+
+#require 'rubygems'
+require 'zip/zipfilesystem'; include Zip
+require 'docsplit'
+
+$create_pdfs = Thread.new{
+  $new_pdfs = []
+  loop{
+    if $new_pdfs.length == 0
+      Thread.stop
+    end
+    dputs 2, "Creating pdfs #{$new_pdfs.inspect}"
+    `date >> /tmp/cp`
+    pdfs = []
+    n_pdfs = $new_pdfs.shift
+    dir = File::dirname( n_pdfs.first )
+    n_pdfs.sort.each{ |p|
+      dputs 3, "Started thread for file #{p} in directory #{dir}"
+      Docsplit.extract_pdf p, :output => dir
+      dputs 5, "Finished docsplit"
+      FileUtils::rm( p )
+      dputs 5, "Finished rm"
+      pdfs.push p.sub( /\.[^\.]*/, '.pdf' )
+    }
+    dputs 3, "Getting #{pdfs.inspect} out of #{dir}"
+    all = "#{dir}/000-all.pdf"
+    psn = "#{dir}/000-4pp.pdf"
+    dputs 3, "Putting it all in one file"
+    `pdftk #{pdfs.join( ' ' )} cat output #{all}`
+    dputs 3, "Putting 4 pages of #{all} into #{psn}"
+    `pdftops #{all} - | psnup -4 -f | ps2pdf -sPAPERSIZE=a4 - #{psn}.tmp`
+    FileUtils::mv( "#{psn}.tmp", psn )
+    dputs 2, "Finished"
+  }
+}
+
+class CourseDiploma < View
+  def layout
+    set_data_class :Courses
+
+    gui_hbox do
+      gui_vbox :nogroup do
+        show_list :grade
+        show_button :do_grades, :print
+      end
+      gui_window :missing_data do
+        show_html :missing
+        show_button :close
+      end
+    end
+    
+    @default_printer = @default_printer ? "-P #{@default_printer}" : ""
+  end
+
+  def rpc_list_choice( session, name, args )
+    dputs 3, "rpc_list_choice with #{name} - #{args.inspect}"
+    ret = reply('empty', ['grade'])
+    case name
+    when "courses"
+      if args['courses'].length > 0
+        course = Entities.Courses.find_by_course_id( args['courses'] )
+        course and ret += reply( 'update', :grade => course.get_pdfs )
+      end
+    end
+    return ret
+  end
+
+  def update_student_diploma( file, student, course )
+    grade = Entities.Grades.find_by_course_person( course.course_id, student.login_name )
+    if grade and grade.to_s != "NP"
+      dputs 3, "New diploma for: #{course.course_id} - #{student.login_name} - #{grade.to_hash.inspect}"
+      ZipFile.open(file){ |z|
+        doc = z.read("content.xml")
+        contents = ""
+        dputs 5, "Contents is: #{course.contents.inspect}"
+        course.contents.split("\n").each{|d|
+          dputs 5, "One line is: #{d}"
+          contents += d + "</text:p></text:list-item><text:list-item><text:p text:style-name='P2'>"
+        }
+        contents.sub!(/(.*)<\/text:p.*/, '\1')
+        dputs 4, "Contents is: #{contents}"
+        doc.gsub!( /_PROF_/, Entities.Persons.login_to_full( course.teacher ) )
+        doc.gsub!( /_RESP_/, Entities.Persons.login_to_full( course.responsible ) )
+        doc.gsub!( /_NOM_/, student.full_name )
+        doc.gsub!( /_DUREE_/, course.duration )
+        doc.gsub!( /_COURS_/, course.description )
+        show_year = course.start.gsub(/.*\./, '' ) != course.end.gsub(/.*\./, '' )
+        doc.gsub!( /_DU_/, course.date_fr( course.start, show_year ) )
+        doc.gsub!( /_AU_/, course.date_fr( course.end ) )
+        doc.gsub!( /_DESC_/, contents )
+        doc.gsub!( /_SPECIAL_/, grade.remark || "" )
+        doc.gsub!( /_MENTION_/, grade.mention )
+        doc.gsub!( /_DATE_/, course.date_fr( course.sign ) )
+        z.file.open("content.xml", "w"){ |f|
+          f.write( doc )
+        }
+        z.commit
+      }
+    else
+      FileUtils.rm( file )
+    end
+  end
+
+  def rpc_button_do_grades( session, args )
+    course_id = args['courses'][0]
+    course = Courses.find_by_course_id(course_id)
+    if not course or course.export_check
+      if course
+        return reply( "window_show", :missing_data ) +
+        reply("update", :missing => course.export_check.join("<br>"))
+      end
+    else
+      students = course.students
+      digits = Math::log10( students.size + 1 ).ceil
+      counter = 1
+      dputs 2, "Diploma_dir is: #{course.diploma_dir}"
+      if not File::directory? course.diploma_dir
+        FileUtils::mkdir( course.diploma_dir )
+      else
+        FileUtils::rm( Dir.glob( course.diploma_dir + "/*" ) )
+      end
+      dputs 2, students.inspect
+      students.each{ |s|
+        student = Entities.Persons.find_by_login_name( s )
+        if student
+          dputs 2, student.login_name
+          student_file = "#{course.diploma_dir}/#{counter.to_s.rjust(digits, '0')}-#{student.login_name}.odt"
+          dputs 2, "Doing #{counter}: #{student.login_name}"
+          FileUtils::cp( "#{Entities.Courses.diploma_dir}/base_gestion.odt", student_file )
+          update_student_diploma( student_file, student, course )
+        end
+        counter += 1
+      }
+      FileUtils::rm( Dir.glob( course.diploma_dir + "/content.xml*" ) )
+      $new_pdfs += [ Dir.glob( course.diploma_dir + "/*odt" ) ]
+      if $new_pdfs.length > 0
+        $create_pdfs.run
+        rpc_list_choice( session, "courses", "courses" => course_id.to_s ) +
+        reply( "auto_update", "-5" )
+      end
+    end
+  end
+  
+  def rpc_update_with_values( session, args )
+    course_id = args['courses'][0]
+    ret = rpc_list_choice( session, "courses", "courses" => course_id.to_s )
+    course = Entities.Courses.find_by_course_id( course_id )
+    if course.get_pdfs.index( "000-4pp.pdf" )
+      ret += reply( :auto_update, 0 )
+    end
+    return ret
+  end
+
+  def rpc_button_close( session, args )
+    reply( "window_hide" )
+  end
+  
+  def rpc_button_print( session, args )
+    if args['grade'].length > 0
+      course_id = args['courses'][0]
+      course = Courses.find_by_course_id(course_id)
+      dputs 2, "Printing #{args['grade'].inspect}"
+      args['grade'].each{|g|
+        `lpr #{@default_printer} #{course.diploma_dir}/#{g}`
+      }
+    end
+  end
+end
---
+++ Views/Course/Modify.rb
@@ -0,0 +1,222 @@
+# Let's you add, modify and delete a course, as well as manage students.
+# It is written to work together with a Moodle-installation. Moodle-configuration
+# over LDAP is:
+# - First name: sn
+# - Name: givenName
+# - E-mail: mail
+# - Town: l
+# - Country: st
+# - Telephone1: mobile
+#
+# Configuration
+# town - the default town
+# country - the default country
+
+class CourseModify < View
+  def layout
+    set_data_class :Courses
+    @update = true
+
+    gui_hbox do
+      gui_vbox :nogroup do
+        gui_window :course do
+          show_list_drop :name_base, "Entities.Courses.list_name_base"
+          show_str :name_date
+          show_button :new_course, :close
+        end
+      end
+      gui_vbox :nogroup do
+        show_block :name
+        show_block :calendar
+        show_block :teacher
+        show_block :accounting
+        show_button :save
+      end
+      gui_vbox :nogroup do
+        show_block :content
+        show_button :print_presence
+        gui_vbox :nogroup do
+          show_list :students
+          show_button :add_students, :bulk_add, :del_student, :edit_student, :print_student
+        end
+      end
+      gui_window :students_win do
+        show_list :students_add, "Entities.Persons.list_students"
+        show_str :search, :gui => %w( update )
+        show_button :new_student, :close
+      end
+      gui_window :students_bulk do
+        show_text :names
+        show_button :bulk_students, :close
+      end
+      gui_window :missing_data do
+        show_html :missing
+        show_button :close
+      end
+    end
+  end
+
+  def rpc_button_delete( session, data )
+    dputs 3, "session, data: #{[session, data.inspect].join(':')}"
+    course = Courses.find_by_course_id( data['courses'][0])
+    dputs 3, "Got #{course.name} - #{course.inspect}"
+    if course
+      dputs 2, "Deleting entry #{course}"
+    course.delete
+    end
+
+    reply( "empty", [:courses] ) +
+    reply( "update", { :courses => Courses.list_courses } )
+  end
+
+  def rpc_button_save( session, data )
+    course = Courses.find_by_name( data['name'] )
+    if course
+      # BUG: they're already saved, don't save it again
+      data.delete( 'students' )
+    course.set_data( data )
+    end
+  end
+
+  def rpc_button_add_students( session, data )
+    reply( "window_show", "students_win" )
+  end
+
+  def rpc_button_bulk_add( session, data )
+    if data['name']
+      reply( "window_show", "students_bulk" )
+    end
+  end
+
+  def update_students( course )
+    reply( "empty_only", [:students] ) +
+    reply( "update", { :students => course.list_students } )
+  end
+
+  def rpc_button_del_student( session, data )
+    course = Courses.find_by_name( data['name'] )
+    data['students'].each{|s|
+      course.students.delete( s )
+    }
+    update_students( course )
+  end
+
+  def rpc_button_edit_student( session, data )
+    dputs 0, "data is: #{data.inspect}"
+    reply( "switch_tab", :PersonModify ) +
+    View.PersonModify.rpc_show( session ) +
+    View.PersonModify.rpc_find( session, :login_name, data["students"][0] )
+  end
+
+  def rpc_button_print_student( session, data )
+    data['students'].each{|s|
+      student = Persons.find_by_login_name( s )
+      dputs 1, "Printing student #{student.full_name}"
+      student.print
+    }
+  end
+
+  def rpc_button_print_presence( session, data )
+    if not Courses.find_by_name( data['name'] ).print_presence
+      reply( "window_show", "missing_data" ) +
+      reply( "update", :missing => "No date or no students")
+    end
+  end
+
+  def rpc_button_new_student( session, data )
+    course = Courses.find_by_name( data['name'] )
+    if course
+      if not course.students.class == Array
+        course.students = []
+      end
+      data['students_add'].each{|s|
+        if not course.students.index( s )
+        course.students.push( s )
+        end
+      }
+      dputs 3, "Students are now: #{course.students.inspect}"
+      update_students( course) +
+      reply( "window_hide" )
+    end
+  end
+
+  # This will add a whole lot of students to the list, creating them and setting
+  # the permissions to "student", but without generating a password
+  # As the creation of a student can take quite some time (10s of seconds),
+  # only one student is created, then the list updated, and a new request is
+  # automatically generated.
+  def rpc_button_bulk_students( session, data )
+    dputs 3, data.inspect
+    course = Courses.find_by_name( data['name'] )
+    users = []
+    if data['names'] and users = data['names'].split("\n")
+      person = Entities.Persons.create( {:first_name => users.shift,
+        :permissions => %w( student ), :town => @town, :country => @country })
+      person.email = "#{person.login_name}@ndjair.net"
+    course.students.push( person.login_name )
+    end
+    if users.length > 0
+      reply( "update", { :names => users.join("\n") } ) +
+      reply( "callback_button", "bulk_students" )
+    else
+      update_students( course ) +
+      reply( "window_hide" )
+    end
+  end
+
+  def rpc_button_add_course( session, data)
+    reply( "window_show", "course" )
+  end
+
+  def rpc_button_new_course( session, data )
+    dputs 3, "session: #{session} - data: #{data.inspect}"
+
+    name = "#{data['name_base'][0]}_#{data['name_date']}"
+    course = Courses.find_by_name( name )
+    if name =~ /_.+/
+      if not course
+        # Search latest course of this type and copy description and contents
+        last_course = Entities.Courses.search_by_name("#{name.gsub( /_.*/, '' )}_.*").sort{|a,b|
+          a.name <=> b.name
+        }.last
+        course = Courses.create( {:name => name })
+        if last_course
+          dputs 2, "Found course #{last_course.name} and copying description and contents"
+        course.description = last_course.description
+        course.contents = last_course.contents
+        end
+      end
+    end
+
+    reply("empty", [:students, :courses]) +
+    reply( "update", {:courses => Entities.Courses.list_courses}) +
+    reply( "update", course.to_hash ) +
+    reply( "update", { :courses => [ course.course_id ] }) +
+    reply( "window_hide" )
+  end
+
+  def rpc_button_close( session, data )
+    reply( "window_hide", "*" )
+  end
+
+  def rpc_list_choice( session, name, *args )
+    #Calling rpc_list_choice with [["courses", {"courses"=>["base_25"], "name_base"=>["base"]}]]
+    dputs 3, "rpc_list_choice with #{name} - #{args.inspect}"
+    if name == "courses" and args[0]['courses'].length > 0
+      course_id = args[0]['courses'][0]
+      dputs 3, "replying for course_id #{course_id}"
+      course = Courses.find_by_course_id(course_id)
+      reply("empty", [:students]) +
+      reply("update", course.to_hash ) +
+      reply("update", {:courses => [course_id] } )
+    else
+      reply("empty", [:students])
+    end
+  end
+
+  def rpc_update( session )
+    reply( 'empty', [:students] ) +
+    super( session )
+  end
+
+end
