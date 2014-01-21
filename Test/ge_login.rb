37e260b73874631c5fa73a5113830adb53b5d0a3
Linus Gasser <ineiti@linusetviviane.ch>
2014-01-21 21:17:27 +0100

    changed ge_login

--- Test/ge_login.rb
+++ Test/ge_login.rb
@@ -1,10 +1,24 @@
 require 'test/unit'
 
-
+class SimulWebReq
+  def self.header
+    {}
+  end
+  
+  def self.peeraddr
+    [0,0,0,0]
+  end
+end
 
 class TC_Login < Test::Unit::TestCase
   def setup
     Entities.delete_all_data()
+
+    dputs(0){"Resetting SQLite"}
+    SQLite.dbs_close_all
+    FileUtils.cp( "db.testGestion", "data/compta.db" )
+    SQLite.dbs_open_load_migrate
+
     Entities.Persons.create( :first_name => "admin", :password => "super123", :permissions => [ "admin" ] )
     Entities.Persons.create( :first_name => "josue", :password => "super", :permissions => [ "secretary" ] )
     Entities.Persons.create( :first_name => "surf", :password => "super", :permissions => [ "internet" ] )
@@ -14,25 +28,25 @@ class TC_Login < Test::Unit::TestCase
   end
 
   def test_login
-    admin = Entities.Persons.find_by_login_name( "admin" )
+    admin = Entities.Persons.match_by_login_name( "admin" )
     assert_not_nil admin, "Couldn't get 'admin'"
 
     reply = RPCQooxdooHandler.request( 1, "View.Welcome", "button", [["default", "login",
-        {"username" => "admin", "password" => "super123" }]])
+        {"username" => "admin", "password" => "super123" }]], SimulWebReq )
     assert_not_nil reply
-    assert_not_equal nil, reply['result'].index{|i| i[:cmd] == "session_id"}, "#{reply.inspect}"
+    assert_not_equal nil, reply['result'].index{|i| i[:cmd] == :session_id}, reply.inspect
 
     reply = RPCQooxdooHandler.request( 1, "View.Welcome", "button", [["default", "login",
         {"username" => "josue", "password" => "false" }]])
     assert_not_nil reply
-    assert_equal [{:data=>"login_failed", :cmd=>"window_show"},
-        {:data=>{:reason=>"Password wrong"}, :cmd=>"update"}], reply['result'], "#{reply.inspect}"
+    assert_equal [{:data=>:login_failed, :cmd=>:window_show},
+        {:data=>{:reason=>"Password wrong"}, :cmd=>:update}], reply['result'], "#{reply.inspect}"
 
     reply = RPCQooxdooHandler.request( 1, "View.Welcome", "button", [["default", "login",
         {"username" => "foo", "password" => "false" }]])
     assert_not_nil reply
-    assert_equal [{:data=>"login_failed", :cmd=>"window_show"},
-        {:data=>{:reason=>"User doesn't exist"}, :cmd=>"update"}], reply['result'], "#{reply.inspect}"
+    assert_equal [{:data=>:login_failed, :cmd=>:window_show},
+        {:data=>{:reason=>"User doesn't exist"}, :cmd=>:update}], reply['result'], "#{reply.inspect}"
   end
 
 end
