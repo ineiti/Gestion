d826c0a46b89924f4ec4bb0cdb70407d854b3508
Linus Gasser <ineiti@linusetviviane.ch>
2014-03-12 21:55:34 +0100

    changed name of reporting to 'report'

---
+++ Views/Compta/Report.rb
@@ -0,0 +1,43 @@
+class ComptaReport < View
+  include VTListPane
+  def layout
+    @visible = true
+    @count = 1
+    @rpc_update = true
+    
+    if Module.constants.index :ACQooxView
+      set_data_class :Accounts
+    end
+    gui_hboxg do
+      gui_vboxg do
+        vtlp_list_entity :account_list, 'Accounts', 'path', 
+          :width => 400, :flex => 1
+      end
+      gui_vbox do
+        show_int :total
+        show_str :desc
+        show_button :report_movements
+      end
+      gui_window :get_report do
+        show_html :txt
+        show_button :close
+      end
+    end
+    
+  end
+  
+  def rpc_button_report_movements( session, data )
+    if acc = data._account_list
+      ddputs(3){"Got account #{acc.path}"}
+      file = "/tmp/report_movs_#{@count += 1}.pdf"
+      acc.print_pdf( file, true )
+      reply( :window_show, :get_report ) +
+        reply( :update, :txt => "Get the report:<br>" +
+          "<a href='#{file}' target='other'>#{file}</a>")
+    end
+  end
+  
+  def rpc_button_close( session, data )
+    reply( :window_hide )
+  end
+end
--- Views/Compta/Show.rb
+++ Views/Compta/Show.rb
@@ -1,43 +1,6 @@
 class ComptaShow < View
   include VTListPane
   def layout
-    @visible = true
-    @count = 1
-    @rpc_update = true
-    
-    if Module.constants.index :ACQooxView
-      set_data_class :Accounts
-    end
-    gui_hbox do
-      gui_vbox do
-        vtlp_list_entity :account_list, 'Accounts', 'path', 
-          :width => 300, :maxheight => 250
-      end
-      gui_vbox do
-        show_int :total
-        show_str :desc
-        show_button :report_movements
-      end
-      gui_window :get_report do
-        show_html :txt
-        show_button :close
-      end
-    end
-    
-  end
-  
-  def rpc_button_report_movements( session, data )
-    if acc = data._account_list
-      ddputs(3){"Got account #{acc.path}"}
-      file = "/tmp/report_movs_#{@count += 1}.pdf"
-      acc.print_pdf( file, true )
-      reply( :window_show, :get_report ) +
-        reply( :update, :txt => "Get the report:<br>" +
-          "<a href='#{file}' target='other'>#{file}</a>")
-    end
-  end
-  
-  def rpc_button_close( session, data )
-    reply( :window_hide )
+    @visible = false
   end
 end
