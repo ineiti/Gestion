6cdb664ad8be799b5333dc8048fdb10f821aebf1
Linus Gasser <ineiti@linusetviviane.ch>
2014-09-08 12:31:52 +0100

    usage-scanning complete

--- Entities/Usage.rb
+++ Entities/Usage.rb
@@ -4,10 +4,19 @@
 
 class Usages < Entities
   def setup_data
-    value_str name
-    value str file_dir
-    value_str file_glob
-    value_str file_filter
+    value_str :name
+    value_str :file_dir
+    value_str :file_glob
+    # The filter can start with s, g, v - f is used to denote fields:
+    # s/search/replace/
+    # gstring_needed
+    # vstring_not_wanted
+    # fs::regexp::arg
+    #  where s is
+    #  - date - arg is a strptime compatible interpretation
+    #  - name - who accessed it - arg can give a command to translate
+    #  - element - what has been accessed
+    value_str :file_filter
   end
 end
 
@@ -15,5 +24,66 @@ class Usage < Entity
   def fetch_files
     return [] unless File.exists? file_dir
     Dir.glob("#{file_dir}/#{file_glob}")
+  end
+
+  def filter_files
+    fetch_files.sort.reverse.map { |logfile|
+      ddputs(3) { "Filtering file #{logfile}" }
+      File.open(logfile, 'r') { |f|
+        f.readlines.map { |l|
+          l.chomp!
+          fields = {}
+          ddputs(4) { "Filtering line #{l}" }
+          file_filter.split(/\n/).each { |filter|
+            ddputs(4) { "Applying filter #{filter.inspect} to #{l.inspect}" }
+            case filter
+              when /^[sgv]/
+                l = Usage.filter_line(l, filter) or break
+              when /^f/
+                field, regex, arg = filter.split('::')
+                fields.merge! Usage.filter_field(l, field[1..-1], regex, arg)
+            end
+            ddputs(4) { "l is now #{l}" }
+          }
+          ( dp fields ).size > 0 ? fields : nil
+        }
+      }
+    }.flatten.compact
+  end
+
+  def self.filter_line(line, filter)
+    case filter
+      when /^s/
+        reg, rep = filter.split('/')[1, 2]
+        ddputs(4) { "Replacing #{reg.inspect} with #{rep.inspect}" }
+        line.sub!(/#{reg}/, rep.to_s)
+      when /^g/
+        ddputs(4) { "grepping #{filter}" }
+        if !(line =~ /#{filter[1..-1]}/)
+          ddputs(4) { "Didn't find - bail out" }
+          line = nil
+        end
+      when /^v/
+        ddputs(4) { "grepping -v #{filter}" }
+        if line =~ /#{filter[1..-1]}/
+          ddputs(4) { 'Found it - bail out' }
+          line = nil
+        end
+    end
+    line
+  end
+
+  def self.filter_field(line, field, regex, arg)
+    ( match = line.match(/#{regex}/) and match.size > 1 ) or return {}
+    ddputs(3){"Found a match: #{match.size} - #{match.inspect}"}
+    value = match[1]
+    case field
+      when /date/
+        {date: Time.strptime(value, arg)}
+      when /name/
+        {name: value}
+      when /element/
+        {element: value}
+    end
   end
 end
--- Test/ge_usage.rb
+++ Test/ge_usage.rb
@@ -1,16 +1,68 @@
 require 'test/unit'
 
 class TC_Usage < Test::Unit::TestCase
-  
+
   def setup
     Entities.delete_all_data()
   end
-  
-  def teardown
-    
-  end
-  
-  def test_usage
 
+  def teardown
+
+  end
+
+  def new_usage(name, dir, glob, filter)
+    Usages.create(:name => name, :file_dir => dir,
+                  :file_glob => glob, :file_filter => filter)
+  end
+
+  def test_usage
+    u1 = new_usage('wiki', 'reports', 'test.log',
+                   "g\\.html HTTP\nfname::.*?([0-9\\.]{7,15}).*")
+    assert_equal( [{name: '192.168.99.146'} ], u1.filter_files )
+
+    time = Time.strptime( '2014-09-05 20:49:02 +0100',
+                          '%Y-%m-%d %H:%M:%S' )
+    u1 = new_usage('wiki', 'reports', 'test.log',
+                   "g\\.html HTTP\nfdate::.*\\[(.*)\\].*::%d/%b/%Y:%H:%M:%S")
+    assert_equal( [{date: time}], u1.filter_files )
+
+    element = 'Portal%253AContents/Portals.html'
+    u1 = new_usage('wiki', 'reports', 'test.log',
+                   "g\\.html HTTP\nfelement::.*GET.*wiki.(.*) HTTP.*")
+    assert_equal( {element: element}, u1.filter_files.first )
+  end
+
+  def test_filter_line
+    logline = 'wiki.profeda.org is the URL'
+    logurl = 'wiki.profeda.org'
+    assert_equal logurl,
+                 Usage.filter_line(logline, 's/ .*//')
+
+    assert_nil Usage.filter_line( logline, 'ghtml')
+    assert_not_nil Usage.filter_line( logline, 'gwiki')
+
+    assert_nil Usage.filter_line( logline, 'vwiki')
+    assert_not_nil Usage.filter_line( logline, 'vhtml')
+  end
+
+  def test_filter_field
+    name = 'foo'
+    assert_equal({},
+                 Usage.filter_field("name is - #{name}",
+                                    'name', '.*:(.*)', nil))
+
+    name = 'foo'
+    assert_equal({name: name},
+                 Usage.filter_field("name is - #{name}",
+                                    'name', '.*- (.*)', nil))
+    date = Date.today
+    assert_equal({date: date.to_time},
+                 Usage.filter_field("Date is - #{date.strftime('%Y%M%D')}",
+                                    'date', '.*- (.*)', '%Y%M%D'))
+
+    element = 'one two three'
+    assert_equal({element: element},
+                 Usage.filter_field("These are my elements - #{element}",
+                                    'element', '.*- (.*)', nil))
   end
 end
