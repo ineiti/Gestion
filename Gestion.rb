24e9955cee802d734a6fb922dd768abf6ef8eb0d
Linus Gasser <ineiti@linusetviviane.ch>
2014-10-24 20:35:03 +0100

    Forgot things with deploy

--- Gestion.rb
+++ Gestion.rb
@@ -7,7 +7,7 @@ Encoding.default_external = Encoding::UTF_8
 # Gestion - a frontend for different modules developed in Markas-al-Nour
 # N'Djaména, Tchad.
 
-VERSION_GESTION='1.7.0'
+VERSION_GESTION='1.8.0'
 require 'fileutils'
 
 GESTION_DIR=File.dirname(__FILE__)
