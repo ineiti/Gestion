2e8a548adbab1811c4dfc5c64233c5e538927f8b
Linus Gasser <ineiti@linusetviviane.ch>
2015-01-22 16:45:31 +0100

    Forgot things with deploy

--- Views/Network/Connection.rb
+++ Views/Network/Connection.rb
@@ -13,6 +13,7 @@ class NetworkConnection < View
         end
         gui_vbox :nogroup do
           show_block :captive
+          show_arg :prerouting, :width => 300
         end
       end
       show_button :save_costs
