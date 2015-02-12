6f2dec033eca1f4dfec20e429f4e6fc13efe6ab7
Linus Gasser <ineiti@linusetviviane.ch>
2015-02-12 09:45:03 +0100

    Forgot things with deploy

---
+++ Views/Admin/Printer.rb
@@ -0,0 +1,36 @@
+class AdminPrinter < View
+  def layout
+    @order = 550
+    @cups_dir = '/etc/cups'
+
+    gui_hbox do
+      gui_vbox do
+        show_list :printers, :single, :flexheight => 1
+        show_button :delete, :add
+      end
+      gui_vbox do
+        show_str :cups_name
+        show_str :device
+        show_str :driver
+        show_button :save
+      end
+    end
+  end
+
+  def get_printers
+    printers = "#{cups_dir}/printers.conf"
+    return [] unless File.exists? printers
+    config = IO.read(printers).split(/^<\/Printer>/)
+    config.collect{|c|
+      cl = c.split("\n")
+      name = cl.select{|l| l =~ /^<Printer/}.match(/^<Printer (.*)>/)[0]
+      device = cl.select{|l| l =~ /^DeviceURI/}.match(/^DeviceURI (.*)>/)[0]
+      driver = cl.select{|l| l =~ /^DeviceURI/}.match(/^DeviceURI (.*)>/)[0]
+    }
+  end
+
+  def rpc_show(session)
+    reply(:empty_all) +
+        reply(:update, :printers => get_printers )
+  end
+end
